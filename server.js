c86e44dadc6f41756b39a44264afb69c1804ea43
luizcanf <luizcanf@gmail.com>
2023-11-30 11:52:52 -0300

    Rolagem do D6 de Ação

--- server.js
+++ server.js
@@ -3,7 +3,12 @@ const app = express()
 app.use(express.urlencoded({ extended: true }))
 app.set("view engine", "ejs")
 
-let dadoAcao = [], d10_1 = [], d10_2 = []
+let d6 = [], d10_1 = [], d10_2 = []
+let rolagensD6 = 0, rolagensD10_1 = 0, rolagensD10_2 = 0
+let rolagemAberta = true
+resetaDado(d6, 6)
+resetaDado(d10_1, 10)
+resetaDado(d10_2, 10)
 
 function resetaDado(dado, lados) {
     for (let i=0; i<lados; i++) {
@@ -17,10 +22,29 @@ app.get('/mestre', (req, res) => {
 })
 
 app.post('/resetaDados', (req, res) => {
-    resetaDado(dadoAcao, 6)
+    resetaDado(d6, 6)
+    rolagensD6 = 0
     resetaDado(d10_1, 10)
+    rolagensD10_1 = 0
     resetaDado(d10_2, 10)
+    rolagensD10_2 = 0
+    rolagemAberta = true
     res.redirect('/mestre')
+})
+
+app.get('/acao', (req, res) => {
+    res.render('rolagem', {dado:'d6', nomeDado: 'Dado de Ação'})
+})
+
+app.post('/d6', (req, res) => {
+    if (rolagemAberta) {
+        roll = Math.floor(Math.random()*6)
+        d6[roll] = d6[roll] + 1
+        rolagensD6++
+        res.render('rolagem', {dado:'d6', nomeDado: 'Dado de Ação', resultado: roll+1, rolagem: rolagensD6})
+    } else {
+        res.render('rolagem', {dado:'d6', nomeDado: 'Dado de Ação', mensagem:'Rolagem de dados bloqueada pelo mestre do jogo.'})
+    }
 })
 
 app.listen(8080, () => {
