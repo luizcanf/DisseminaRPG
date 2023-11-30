b7e9a808566a8939f03a2f0990e9648fcb010116
luizcanf <luizcanf@gmail.com>
2023-11-30 17:00:36 -0300

    Separação das rolagens dos 3 tipos de dados

--- server.js
+++ server.js
@@ -61,11 +61,11 @@ app.post('/exibeRolagem', (req, res) => {
     else if (total > desafio1 && total > desafio2)
         resolucao = 'Acerto Forte!'
     else if (total <= desafio1 && total <= desafio2 && desafio1 == desafio2)
-        resolução = 'Erro Crítico!'
+        resolucao = 'Erro Crítico!'
     else if (total <= desafio1 && total <= desafio2)
-        resolução = 'Erro!'
+        resolucao = 'Erro!'
     else
-        resolução = 'Acerto Fraco.'
+        resolucao = 'Acerto Fraco.'
 
     total = `${acao} + ${bonus} = ${total}`
     res.render('resultado', {total, desafio1, desafio2, resolucao})
@@ -83,6 +83,39 @@ app.post('/d6', (req, res) => {
         res.render('rolagem', {dado:'d6', nomeDado: 'Dado de Ação', resultado: roll+1, rolagem: rolagensD6})
     } else {
         res.render('rolagem', {dado:'d6', nomeDado: 'Dado de Ação', mensagem:'Rolagem de dados bloqueada pelo mestre do jogo.'})
+    }
+})
+
+app.get('/desafio1', (req, res) => {
+    res.render('rolagem', {dado:'d10_1', nomeDado: 'Dado de Desafio 1'})
+})
+
+app.post('/d10_1', (req, res) => {
+    if (rolagemAberta) {
+        roll = Math.floor(Math.random()*10)
+        d10_1[roll] = d10_1[roll] + 1
+        rolagensD10_1++
+        roll3 = Math.floor(Math.random()*10)
+        d10_2[roll3] = d10_2[roll3] + 1
+        rolagensD10_2++
+        res.render('rolagem', {dado:'d10_1', nomeDado: 'Dado de Desafio 1', resultado: roll+1, rolagem: rolagensD10_1})
+    } else {
+        res.render('rolagem', {dado:'d10_1', nomeDado: 'Dado de Desafio 1', mensagem:'Rolagem de dados bloqueada pelo mestre do jogo.'})
+    }
+})
+
+app.get('/desafio2', (req, res) => {
+    res.render('rolagem', {dado:'d10_2', nomeDado: 'Dado de Desafio 2'})
+})
+
+app.post('/d10_2', (req, res) => {
+    if (rolagemAberta) {
+        roll = Math.floor(Math.random()*10)
+        d10_2[roll] = d10_2[roll] + 1
+        rolagensD10_2++
+        res.render('rolagem', {dado:'d10_2', nomeDado: 'Dado de Desafio 2', resultado: roll+1, rolagem: rolagensD10_2})
+    } else {
+        res.render('rolagem', {dado:'d10_2', nomeDado: 'Dado de Desafio 2', mensagem:'Rolagem de dados bloqueada pelo mestre do jogo.'})
     }
 })
 
